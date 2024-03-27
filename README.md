aa318370190844eef0aa2c7ffaf0db02dfc453e2
Munna8383 <jahedmunna612@gmail.com>
2024-03-27 14:45:07 +0600

    update readme

--- README.md
+++ README.md
@@ -1,3 +1,26 @@
+
+# Book Heaven
+
+A online book website for reading different kinds of book.
+
+
+## Website Live Link:
+
+*Live Website:* [https://book-heaven122.netlify.app/](https://book-heaven122.netlify.app/)
+
+
+## Features
+
+- Explore Wide Range of books.
+- Can Check Deep Details Of The book.
+- You Can Add book to ReadList Or WishList.
+- Can read book in Mobile devices.
+- Explore Bar Charts To Idea About book.
+
+
+
+
+
 # React + Vite
 
 This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
--- src/Component/Navbar/Navbar.jsx
+++ src/Component/Navbar/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
 <header className="p-4  dark:text-gray-800">
 	<div className="container flex items-center justify-between">
 		<h1 className="text-3xl font-extrabold hidden lg:block">Book Heaven</h1>
-		<ul className=" gap-5 flex items-center justify-center">
+		<ul className=" gap-5 flex flex-wrap items-center justify-center">
 			<li className="flex">
 				<NavLink to={"/"}  className={({isActive})=>isActive?"text-green-500 border-2 rounded-xl border-green-500 p-1":" "}>Home</NavLink>
 			</li>
@@ -26,7 +26,7 @@ const Navbar = () => {
 			</li>
 		</ul>
 		<div className="items-center flex-shrink-0 gap-3  hidden lg:flex">
-        <button className="btn text-white btn-success">Sign IN</button>
+        <button className="btn text-white btn-success">Sign In</button>
         <button className="btn text-white btn-active btn-accent">Sign Up</button>
 		</div>
 		
